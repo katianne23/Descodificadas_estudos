0c654a2bf805a440df6874d3491baeae21fd152e
Katianne23 <katianne40@gmail.com>
2023-06-03 19:15:42 -0300

    Variaveis

---
+++ Variaveis/script.js
@@ -0,0 +1,25 @@
+alert("Pagina Carregada com Sucesso!")
+
+var valor1 = 10;
+var valor2 = 15;
+
+const soma = valor1  + valor2;
+const subtração =  valor1 - valor2;
+const multiplicação = valor1 * valor2;
+const compare = valor1 === valor2;
+const menorIgual = valor1 <= valor2;
+const maiorIgual = valor1 <= valor2;
+
+document.write(soma);
+document.write(" ");
+document.write(subtração);
+document.write(" ");
+document.write(multiplicação);
+document.write(" ");
+document.write(compare);
+document.write(" ");
+document.write(menorIgual);
+document.write(" ");
+document.write(maiorIgual);
+
+
